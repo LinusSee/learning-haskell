3846c993188eacdcf3d0537a260dce21779d2e16
Linus <linussee@web.de>
2019-12-02 22:58:32 +0100

    Maximum of list function

--- exercise_functions.hs
+++ exercise_functions.hs
@@ -2,3 +2,12 @@ fibonacci   :: Integer -> Integer
 fibonacci 0 =  0
 fibonacci 1 =  1
 fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+
+myMaximum        :: (Ord a) => [ a ] -> a
+myMaximum []     = error "Empty list can't have a maximum"
+myMaximum [ x ]  =  x
+myMaximum (x:xs)
+    | x > maxTail = x
+    | otherwise   = maxTail
+    where maxTail = myMaximum xs
