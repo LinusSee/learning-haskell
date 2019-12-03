b50e33ef678272338635f1860abaf638275711ce
Linus <linussee@web.de>
2019-12-03 17:32:55 +0100

    Reverse function

--- exercise_functions.hs
+++ exercise_functions.hs
@@ -17,3 +17,14 @@ replicate'     :: (Num i, Ord i) => i -> i -> [i]
 replicate' n x
     | n <= 0    = []
     | otherwise = x:replicate' (n - 1) x
+
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' _ []     = []
+take' n (x:xs)
+    | n <= 0    = []
+    | otherwise = x:take' (n - 1) xs
+
+reverse' :: (Ord i) => [i] -> [i]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
