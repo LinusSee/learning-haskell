2797f9428b8902edd0d183be676abbd97843496a
Linus <linussee@web.de>
2019-12-13 23:27:45 +0100

    Merge row neighbours

--- try_on_game_of_life/module.hs
+++ try_on_game_of_life/module.hs
@@ -7,8 +7,8 @@ val = [
 
 
 neighbours :: [Bool] -> [Int]
-neighbours [] = [0]
-neighbours [x] = [0]
+neighbours [] = []
+neighbours [x] = []
 neighbours (x:y:xs)
   | x && y    = [1, 1] ++ next_neighbours
   | x         = [0, 1] ++ next_neighbours
@@ -17,8 +17,16 @@ neighbours (x:y:xs)
   where next_neighbours = neighbours (y:xs)
 -- Odd cases / first two matches should probably be an empty list
 
+merge :: [Int] -> [Int]
+merge [] = []
+merge [x] = [x]
+merge [x, y] = [x, y]
+merge [w, x, y, z] = [w, x + y, z]
+merge (x:y:w:xs) = [start, acc] ++ merge (w:xs)
+  where start = x
+        acc = y + w
 
 main :: IO ()
 --main = putStrLn "Hello, world :)"
-main = let mapped = map neighbours val
+main = let mapped = map (merge . neighbours) val
        in putStrLn . show $ mapped
