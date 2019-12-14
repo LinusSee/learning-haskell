806dc5e2e326886c63b55edeea047ff7b4b9f550
Linus <linussee@web.de>
2019-12-14 17:09:14 +0100

    Removed code (idea wasn't working)

--- try_on_game_of_life/module.hs
+++ try_on_game_of_life/module.hs
@@ -6,27 +6,9 @@ val = [
     ]
 
 
-neighbours :: [Bool] -> [Int]
-neighbours [] = []
-neighbours [x] = []
-neighbours (x:y:xs)
-  | x && y    = [1, 1] ++ next_neighbours
-  | x         = [0, 1] ++ next_neighbours
-  | y         = [1, 0] ++ next_neighbours
-  | otherwise = [0, 0] ++ next_neighbours
-  where next_neighbours = neighbours (y:xs)
--- Odd cases / first two matches should probably be an empty list
+neighbours :: [[Bool]] -> [[Int]]
 
-merge :: [Int] -> [Int]
-merge [] = []
-merge [x] = [x]
-merge [x, y] = [x, y]
-merge [w, x, y, z] = [w, x + y, z]
-merge (x:y:w:xs) = [start, acc] ++ merge (w:xs)
-  where start = x
-        acc = y + w
+
 
 main :: IO ()
---main = putStrLn "Hello, world :)"
-main = let mapped = map (merge . neighbours) val
-       in putStrLn . show $ mapped
+main = putStrLn "Hello, world :)"
