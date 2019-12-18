f8346c5f8c192fbbf5a944c3951e1e47adeedcf6
Linus <linussee@web.de>
2019-12-18 17:30:12 +0100

    Finished spock rest tutorial

--- rest-example/spock-rest/app/Main.hs
+++ rest-example/spock-rest/app/Main.hs
@@ -26,31 +26,55 @@ import           GHC.Generics
 import           Control.Monad.Logger     (LoggingT, runStdoutLoggingT)
 import           Database.Persist         hiding (get)
 import qualified Database.Persist         as P
+import           Database.Persist.Sqlite  hiding (get)
 import           Database.Persist.TH
 
 
-data Person = Person
-    { name :: Text
-    , age  :: Int
-    } deriving (Generic, Show)
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person json
+  name Text
+  age Int
+  deriving Show
+|]
 
-instance ToJSON Person
-instance FromJSON Person
 
-
-type Api = SpockM () () () ()
-type ApiAction a = SpockAction () () () a
+type Api = SpockM SqlBackend () () ()
+type ApiAction a = SpockAction SqlBackend () () a
 
 
 main :: IO ()
 main = do
-  spockConfig <- defaultSpockCfg () PCNoDatabase ()
+  pool <- runStdoutLoggingT $ createSqlitePool "api.db" 5
+  spockConfig <- defaultSpockCfg () (PCPool pool) ()
+  runStdoutLoggingT  $ runSqlPool (runMigration migrateAll) pool
   runSpock 8080 (spock spockConfig app)
 
 app :: Api
 app = do
-  get "people" $
-    json [ Person { name="Fry", age=25 }, Person { name="Bender", age=4 } ]
+  get "people" $ do
+    allPeople <- runSQL $ selectList [] [Desc PersonId]
+    json allPeople
+  get ("people" <//> var) $ \personId -> do
+    maybePerson <- runSQL $ P.get personId :: ApiAction (Maybe Person)
+    case maybePerson of
+      Nothing -> errorJson 2 "Could not find a person with a matching id"
+      Just thePerson -> json thePerson
   post "people" $ do
-    thePerson <- jsonBody' :: ApiAction Person
-    text $ "Parsed: " <> pack (show thePerson)
+    maybePerson <- jsonBody :: ApiAction (Maybe Person)
+    case maybePerson of
+      Nothing -> errorJson 1 "Failed to parse request body as a person"
+      Just thePerson -> do
+        newId <- runSQL $ insert thePerson
+        json $ object ["result" .= String "success", "id" .= newId]
+
+
+runSQL :: (HasSpock m, SpockConn m ~ SqlBackend) => SqlPersistT (LoggingT IO) a -> m a
+runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
+
+
+errorJson:: Int -> Text -> ApiAction ()
+errorJson code message = json $
+  object
+  [ "result" .= String "failure"
+  , "error"  .= object ["code" .= code, "message" .= message]
+  ]
