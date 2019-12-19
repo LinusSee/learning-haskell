3c8a3411cf17b102dae27cac3f78f0e405119cd5
Linus <linussee@web.de>
2019-12-19 20:01:59 +0100

    HTML via lucid and server state

--- rest-example/example2/notekeeper/app/Main.hs
+++ rest-example/example2/notekeeper/app/Main.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main where
 
-import Lib
+import Data.IORef
+import Data.Text          (Text)
+import Web.Spock
+import Web.Spock.Config
+
+import Lucid
+import Web.Spock.Lucid    (lucid)
+
+
+type Server a = SpockM () () ServerState a
+
+newtype ServerState = ServerState { notes :: IORef [Note] }
+
+data Note = Note {
+    author :: Text
+  , contents :: Text }
+
+
+app :: Server ()
+app = get root $ lucid $ do
+  h1_ "Hello!"
+  p_  "How are you today?"
 
 main :: IO ()
-main = someFunc
+main = do
+  st <- ServerState <$>
+    newIORef [ Note "Alice" "Must not forget to walk dog."
+      , Note "Bob" "Must. Eat. Pizza!!"
+    ]
+  cfg <- defaultSpockCfg () PCNoDatabase st
+  runSpock 8080 (spock cfg app)
